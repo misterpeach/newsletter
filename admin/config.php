c7bdcef9fe748bfe021da6341054dd9476621816
misterpeach <pch@misterpeach.de>
2021-06-19 14:04:12 +0200

    first push

---
+++ admin/config.php
@@ -0,0 +1,17 @@
+<?php
+
+  define('DB_SERVER', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASSWORD', '');
+  define('DB_NAME', 'newsletter');
+
+  define('FROM_EMAIL', 'no_reply@ohyeahemail.com');
+  define('FROM_NAME', 'oh yeah email!');
+
+  session_start();
+  require_once 'classes.php';
+  $mini = false;
+  $nonav = false;
+  error_reporting(0);
+
+?>
---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<?php
+
+  require_once 'config.php';
+  $title = "Home!";
+  $content = <<<EOF
+  <h3>current stats</h3>
+  Our home page!
+  EOF;
+  include 'layout.php';
+
+?>
